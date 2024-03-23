b161f91d599350a9843018c8b069fa023325812d
qwerty142 <62697835+qwerty142@users.noreply.github.com>
2024-03-23 21:36:37 +0300

    small_fix

--- lab1/src/main/java/lab/dto/BankAdmin.java
+++ lab1/src/main/java/lab/dto/BankAdmin.java
@@ -1,9 +1,12 @@
 package lab.dto;
 
+import lab.domain.IBankAccount;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 @Log4j2
@@ -22,16 +25,16 @@ public class BankAdmin {
     }
 
     public void banAccount(long userId, long accountId, UserRepository repository) {
-        IBankAccount account = repository
+        Optional<IBankAccount> account = repository
                 .getUserAccounts().
                 get(userId)
                 .stream()
-                .filter(account -> account.getAccountId() == accountId)
+                .filter(account1 -> account1.getAccountId() == accountId)
                 .findFirst();
         if(account.isEmpty()) {
             log.error("Такого пользователя не существует");
         }
 
-        account.setBlockStatus(true);
+        account.get().setBlockStatus(true);
     }
 }
